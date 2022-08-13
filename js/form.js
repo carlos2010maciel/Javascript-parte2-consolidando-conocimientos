6ca44370760f9489957191ca75053ad88b5dc4c2
Horacio Carlos Maciel de Lima <95258782+carlos2010maciel@users.noreply.github.com>
2022-08-13 01:17:58 -0300

    Agregada funciòn buscar con AJAX
    Buscar pacientes con AJAX y traerlos desde una API externa

--- js/form.js
+++ js/form.js
@@ -1,54 +1,58 @@
 var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-botonAdicionar.addEventListener("click", function (event) {
+botonAdicionar.addEventListener("click",function(event){
     event.preventDefault();
 
-    var form = document.querySelector("#form-adicionar");
-    var paciente = capturarDatosPacientes(form);
-    var pacienteTr = construirTr(paciente);
-
+    var form = document.querySelector("#form-adicionar"); 
+    var paciente = capturarDatosPaciente(form);
+    
+    
     var errores = validarPaciente(paciente);
 
-    if (errores.length > 0) {
+    if(errores.length > 0){
         exhibirMensajesErrores(errores);
-        return; //hace alusión a la función anónima
+        return;
     }
-    //Validar paciente
-    var tabla = document.querySelector("#tabla-pacientes");
-    tabla.appendChild(pacienteTr);
-    form.reset(); //Para que luego de hacerclick el formulario quede vacío
+
+    adicionarPacienteEnLaTabla(paciente);
+    form.reset();
 
     var mensajesErrores = document.querySelector("#mensajes-errores");
     mensajesErrores.innerHTML = "";
 
 });
 
-function capturarDatosPacientes(form) {
-    //capturando los datos del formulario
+function adicionarPacienteEnLaTabla(paciente){
+    var pacienteTr = construirTr(paciente);
+    var tabla = document.querySelector("#tabla-pacientes");
+    tabla.appendChild(pacienteTr);
+}
 
-    var paciente = { //creo la clase paciente
+
+function capturarDatosPaciente(form){
+    //capturando los datos del formulario
+    var paciente = {
         nombre: form.nombre.value,
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calcularIMC(form.peso.value, form.altura.value)
+        imc: calcularIMC(form.peso.value,form.altura.value)
     }
-
-    return paciente;
+    return paciente; 
 }
 
-function construirTr(paciente) {
-    var pacienteTr = document.createElement("tr");
-    pacienteTr.classList.add("paciente");
+function construirTr(paciente){
+     
+       var pacienteTr = document.createElement("tr");     
+       pacienteTr.classList.add("paciente");
+          
+       pacienteTr.appendChild(construirTd(paciente.nombre,"info-nombre"));
+       pacienteTr.appendChild(construirTd(paciente.peso,"info-peso"));
+       pacienteTr.appendChild(construirTd(paciente.altura,"info-altura"));
+       pacienteTr.appendChild(construirTd(paciente.gordura,"info-gordura"));
+       pacienteTr.appendChild(construirTd(paciente.imc,"info-imc"));
 
-    //Asignación al tr de los td, y la tabla el tr
-    pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre"));
-    pacienteTr.appendChild(construirTd(paciente.peso, "info-peso"));
-    pacienteTr.appendChild(construirTd(paciente.altura, "info-altura"));
-    pacienteTr.appendChild(construirTd(paciente.gordura, "info-gordura"));
-    pacienteTr.appendChild(construirTd(paciente.imc, "info-imc"));
-
-    return pacienteTr;
+       return pacienteTr; 
 }
 
 function construirTd(dato, clase) {
@@ -64,31 +68,24 @@ function validarPaciente(paciente) {
     if (paciente.nombre.length == 0) {
         errores.push("El nombre no puede estar vacío");
     }
-
     if (paciente.peso.length == 0) {
         errores.push("El peso no puede estar vacío");
     }
-
     if (paciente.altura.length == 0) {
         errores.push("La altura no puede estar vacía");
     }
-
     if (paciente.gordura.length == 0) {
         errores.push("El %gordura no puede estar vacío ni contener valores negativos");
     }
-
-
     if (!validarPeso(paciente.peso)) {
         errores.push("El peso es incorrecto");
     }
     if (!validarAltura(paciente.altura)) {
         errores.push("La altura es incorrecta");
     }
-
     if (!validarGordura(paciente.gordura)) {
         errores.push("El % de gordura es incorrecto");
     }
-
     return errores;
 }
 
